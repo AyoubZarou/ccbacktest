fb0a410e46492ee3d4d9614c24654c97fb513a8b
AyoubZarou <34717975+AyoubZarou@users.noreply.github.com>
2020-01-07 12:41:10 +0100

    add to_pandas parameter to step an apply methods

--- pipeline/pipelines.py
+++ pipeline/pipelines.py
@@ -22,13 +22,21 @@ class FactorPipeline(Pipeline):
         self.factor = factor
         self._name = self.factor.name
 
-    def apply(self, df) -> Dict[str, dict]:
+    def apply(self, df, to_pandas: bool = False) -> Dict[str, dict]:
         factor = self.factor
-        return {self.name: factor.apply(df)}
+        return_dict = {self.name: factor.apply(df)}
+        if not to_pandas:
+          return return_dict
+        else:
+          pass
 
-    def step(self, series: pd.Series) -> Dict[str, dict]:
-        actor = self.factor
-        return {self.name: factor.step(series)}
+    def step(self, series: pd.Series, to_pandas: bool = False) -> Dict[str, dict]:
+        factor = self.factor
+        return_dict = {self.name: factor.step(series)}
+        if not to_pandas:
+          return return_dict
+        else:
+          pass
 
 
 class UnionPipeline(Pipeline):
@@ -39,15 +47,17 @@ class UnionPipeline(Pipeline):
         self._pipelines = pipelines
         self._name = name
 
-    def apply(self, df: pd.DataFrame) -> Dict[str, dict]:
+    def apply(self, df: pd.DataFrame, to_pandas: bool = False) -> Dict[str, dict]:
       dicts = [p.apply(df) for p in self._pipelines]
       union_dict = _union_dicts(dicts)
       return {self.name: union_dict} 
 
-    def step(self, series: pd.Series) -> Dict[str, dict]:
+    def step(self, series: pd.Series, to_pandas: bool = False) -> Dict[str, dict]:
       dicts = [p.step(series) for p in self._pipelines]
       union_dict = _union_dicts(dicts)
-      return {self.name: union_dict} 
+      return_dict = {self.name: union_dict} 
+      if not to_pandas:
+        return return_dict
 
 
 class MultiFactorPipeline(Pipeline):
@@ -55,10 +65,16 @@ class MultiFactorPipeline(Pipeline):
         self._name = name
         self._factors = factors
 
-    def apply(self, df: pd.DataFrame) -> Dict[str, dict]:
-        union_dict = {factor.name: factor.apply(df) for factor in self._factors}
-        return {self.name: union_dict}
+    def apply(self, df: pd.DataFrame, to_pandas: bool = False) -> Dict[str, dict]:
+        dicts = [{factor.name: factor.apply(df)} for factor in self._factors] 
+        union_dict = _union_dicts(dicts)
+        return_dict = {self.name: union_dict}
+        if not to_pandas:
+          return return_dict
 
-    def step(self, series: pd.Series) -> Dict[str, dict]:
-        union_dict = {factor.name: factor.step(series) for factor in self._factors}
-        return {self.name: union_dict}
+    def step(self, series: pd.Series, to_pandas: bool = False) -> Dict[str, dict]:
+        dicts = [{factor.name: factor.step(series)} for factor in self._factors] 
+        union_dict = _union_dicts(dicts)
+        return_dict = {self.name: union_dict}
+        if not to_pandas:
+          return return_dict
