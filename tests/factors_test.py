3a41d598411587720a8cb99e2b215bbbc73053a6
AyoubZarou <34717975+AyoubZarou@users.noreply.github.com>
2020-01-08 12:17:19 +0100

    generating test dataframe

--- tests/factors_test.py
+++ tests/factors_test.py
@@ -4,3 +4,15 @@ import unitest
 
 from ccbacktest.factors.factors import LambdaFactor, MACD, MA, RSI, VWAP
 
+
+# generating a test df
+n = 100
+volume = np.random.random(n) * 10 + 10
+open_ =  100 + 10 * np.random.random(n)
+close = 100 + 10 * np.random.random(n)
+high = np.maximum(open_, close) + np.random.random() * 4
+low = np.minimum(open_, close) - np.random.random() * 4
+open_time = np.arange(n)
+test_df = pd.DataFrame(dict(open_time=open_time, open=open_,
+                      high=high, low=low, close=close, volume=volume))
+
