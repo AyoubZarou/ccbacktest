3276c84df0e1a2f33158c0e843b8a93176dc0d23
AyoubZarou <34717975+AyoubZarou@users.noreply.github.com>
2020-01-08 12:29:42 +0100

    starting a test case for MA

--- tests/factors_test.py
+++ tests/factors_test.py
@@ -16,3 +16,12 @@ open_time = np.arange(n)
 test_df = pd.DataFrame(dict(open_time=open_time, open=open_,
                       high=high, low=low, close=close, volume=volume))
 
+
+class MATest(unitest.TestCase):
+  def __init__(self):
+    self.to_apply = test_df.iloc[:50, :]
+    self.to_step = test_df.iloc[50, :]
+    self.ma = MA(6) 
+  def test_apply_and_step():
+    pass
+  
